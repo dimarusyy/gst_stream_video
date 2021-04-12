99a3a9546128c13c3739dd9c90fd46998a377241
Dima Rusyy <dima.ru.com@gmail.com>
2021-04-12 09:03:35 +0300

    Add project files.

---
+++ gst_stream_video/gst_stream_video.cpp
@@ -0,0 +1,12 @@
+﻿// gst_stream_video.cpp : Defines the entry point for the application.
+//
+
+#include "gst_stream_video.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
